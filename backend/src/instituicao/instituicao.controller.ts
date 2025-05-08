95948e259534dd90fc45995b0d2922520ecf2a02
jpkm17 <jpm83633@gmail.com>
2025-05-08 09:44:48 -0300

    Crud Insituicao

--- backend/src/instituicao/instituicao.controller.ts
+++ backend/src/instituicao/instituicao.controller.ts
@@ -1,34 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
 import { InstituicaoService } from './instituicao.service';
 import { CreateInstituicaoDto } from './dto/create-instituicao.dto';
 import { UpdateInstituicaoDto } from './dto/update-instituicao.dto';
+import { Instituicao } from './entities/instituicao.entity';
 
 @Controller('instituicao')
 export class InstituicaoController {
   constructor(private readonly instituicaoService: InstituicaoService) {}
 
-  @Post()
-  create(@Body() createInstituicaoDto: CreateInstituicaoDto) {
+  @Post('create')
+  create(@Body() createInstituicaoDto: CreateInstituicaoDto): Promise<Instituicao> {
     return this.instituicaoService.create(createInstituicaoDto);
   }
 
-  @Get()
-  findAll() {
+  @Get('all')
+  findAll(): Promise<Instituicao[]> {
     return this.instituicaoService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get('getOne/:id')
+  findOne(@Param('id') id: string): Promise<Instituicao> {
     return this.instituicaoService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateInstituicaoDto: UpdateInstituicaoDto) {
+  @Put('update/:id')
+  update(@Param('id') id: string, @Body() updateInstituicaoDto: UpdateInstituicaoDto): Promise<Instituicao> {
     return this.instituicaoService.update(+id, updateInstituicaoDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete('remove/:id')
+  remove(@Param('id') id: string): Promise<Instituicao> {
     return this.instituicaoService.remove(+id);
   }
 }
