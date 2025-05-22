e6280e0219b3a4ae6d3659f7f1e94a791bfbf84e
jpkm17 <jpm83633@gmail.com>
2025-05-22 08:12:15 -0300

    ADD: Criar campanha

--- backend/src/instituicao/instituicao.service.ts
+++ backend/src/instituicao/instituicao.service.ts
@@ -4,14 +4,17 @@ import { UpdateInstituicaoDto } from './dto/update-instituicao.dto';
 import { Instituicao } from './entities/instituicao.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Campanha } from './entities/campanha.entity';
+import { CreateCampanhaDto } from './dto/create-campanha.dto';
 
 @Injectable()
 export class InstituicaoService {
   constructor(
     @InjectRepository(Instituicao)
     private instituicaoRepository: Repository<Instituicao>,
+    @InjectRepository(Campanha)
+    private campanhaRepository: Repository<Campanha>,
   ) { }
-
 
   async create(createInstituicaoDto: CreateInstituicaoDto): Promise<Instituicao> {
     const instituicao = await this.instituicaoRepository.create(createInstituicaoDto)
@@ -26,7 +29,7 @@ export class InstituicaoService {
   async findOne(id: number): Promise<Instituicao> {
     const instituicao = await this.instituicaoRepository.findOneBy({ id_instituicao: id })
 
-    if(!instituicao) throw new NotFoundException('Instituição não encontrada')
+    if (!instituicao) throw new NotFoundException('Instituição não encontrada')
 
     return instituicao
   }
@@ -34,8 +37,8 @@ export class InstituicaoService {
   async update(id: number, updateInstituicaoDto: UpdateInstituicaoDto): Promise<Instituicao> {
     const instituicao = await this.instituicaoRepository.findOneBy({ id_instituicao: id })
 
-    if(!instituicao) throw new NotFoundException('Instituição não encontrada')
-    
+    if (!instituicao) throw new NotFoundException('Instituição não encontrada')
+
     Object.assign(instituicao, updateInstituicaoDto)
 
     return instituicao
@@ -44,8 +47,34 @@ export class InstituicaoService {
   async remove(id: number): Promise<Instituicao> {
     const instituicao = await this.instituicaoRepository.findOneBy({ id_instituicao: id })
 
-    if(!instituicao) throw new NotFoundException('Instituição não encontrada')
+    if (!instituicao) throw new NotFoundException('Instituição não encontrada')
 
     return await this.instituicaoRepository.remove(instituicao)
   }
+
+  async createCampaing(createCampanhaDto: CreateCampanhaDto): Promise<Campanha> {
+    const campanha = await this.campanhaRepository.create(createCampanhaDto)
+
+    return campanha;
+  }
+
+
+  async findAllCampaignsByInstituicao(id: number): Promise<Campanha[]> {
+    const campanhas = await this.campanhaRepository.find({
+      where: { instituicao: { id_instituicao: id } },
+      relations: ['instituicao'],
+    });
+
+    return campanhas;
+  }
+
+
+  async removeCampaing(id: number): Promise<Campanha> {
+    const campanha = await this.campanhaRepository.findOneBy({ id: id })
+
+    if (!campanha) throw new NotFoundException('campanha não encontrada')
+
+    return await this.campanhaRepository.remove(campanha)
+  }
+
 }
